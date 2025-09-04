18e1aeff9544a338202f948d20802a661074a3a4
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-09-04 20:13:46 +0530

    For fuck sake work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Preferences from "./pages/Preferences";
@@ -7,18 +6,17 @@ import Journal from "./pages/Journal";
 import "./App.css";
 
 function App() {
-  document.addEventListener("contextmenu", (e) => e.preventDefault());
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/preferences" element={<Preferences />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/journal" element={<Journal />} />
-      </Routes>
-    </Router>
-  );
+    document.addEventListener("contextmenu", (e) => e.preventDefault());
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/preferences" element={<Preferences />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/journal" element={<Journal />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
-
